8c810e13b62df90b8a99952eb8b2e08b21ae0cf3
John O'Neill <jack_t_oneill@yahoo.com>
2021-04-21 13:12:16 -0400

    Added Game() class
    Created a class that, given two players, can simulate a battle between them.
    - Initialization determines who goes first
    - simulate() function, given a depth value, will emulate and display results of an offensive/defensive decision across that number of turns

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # main.py
+from copy import deepcopy
 
 class Player():
 	def __init__(self, super_name, real_name, gender, species, height, weight, inte, stre, spee, dura, powe, comb, p_type):
@@ -36,20 +37,59 @@ class Player():
 			damage *= ratio
 		target.health = max(0, round(target.health - damage, 1)) # health will not drop below zero
 		
+
+class Game():
+	def __init__(self, player_one, player_two):
+		self.p1 = player_one
+		self.p2 = player_two
+		self.turn = 0 if ((self.p1.speed * 0.5 + self.p1.intelligence * 0.5) >= (self.p2.speed * 0.5 + self.p2.intelligence * 0.5)) else 1
+		# 0 if p1 goes first, 1 if p2 goes first
+		self.offense = None
+		self.defense = None
+		self.p1.print_stats()
+		self.p2.print_stats()
+	def simulate(self, depth):
+		print("Depth remaining:\t" + str(depth) + "\t Healths left:\t"  + str(self.p1.health) + " " + str(self.p2.health))
+		if (self.p1.health <= 0):
+			print("Btw, looks like p2 won!")
+			return
+		if (self.p2.health <= 0):
+			print("Btw, looks like p1 won!")
+			return
+		if (depth > 0):
+			self.offense = deepcopy(self)
+			self.defense = deepcopy(self)
+			self.offense.turn = 0 if self.turn else 1
+			self.defense.turn = 0 if self.turn else 1
+			if (self.turn == 0):
+				self.offense.p1.strike(self.offense.p2, 0)
+			else:
+				self.offense.p2.strike(self.offense.p1, 0)
+			if (self.turn == 1):
+				self.defense.p1.strike(self.defense.p2, 1)
+			else:
+				self.defense.p2.strike(self.defense.p1, 1)
+			self.offense.simulate(depth - 1)
+			self.defense.simulate(depth - 1)
+		
+		
+			
+			
+		
+		
+		
+
 # Mock battle
+
+
+
 
 player_one = Player("Frog Man", "Joe Schmoe", "Male", "Froganoid", 175, 90.0, 25, 75, 60, 60, 60, 20, 0)
 player_two = Player("The Doktor", "Darryl Ichtenstein", "Male", "Human", 175, 50.0, 90, 20, 70, 30, 70, 50, 1)
-player_one.print_stats()
-player_two.print_stats()
 
-player_two.strike(player_one, 1) # 1 means block
-print(str(player_one.super_name) + " is struck by " + str(player_two.super_name) + "!\n")
-player_one.print_stats()
-player_one.strike(player_two, 0) # 0 means no block
-print(str(player_two.super_name) + " is struck by " + str(player_one.super_name) + "!\n")
-player_two.print_stats()
+the_game = Game(player_one, player_two)
 
+the_game.simulate(3)
 
 """
 Win condition:
